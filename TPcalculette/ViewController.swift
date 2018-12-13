92c74eb833b1e230220949f8e5a1ca24ed95ea7a
qr0wproject <fclaeys.project@gmail.com>
2018-12-13 19:18:58 +0100

    Initial Commit

---
+++ TPcalculette/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TPcalculette
+//
+//  Created by qr0w on 13/12/2018.
+//  Copyright © 2018 qr0w. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
